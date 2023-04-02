2da0ebdfd6f363c13b7a2ebfb11bb7ddaed98f0c
Mikiiiii <suloski11@gmail.com>
2023-04-02 13:36:35 +0200

    Init project

---
+++ Level plus - Team 23/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Level_plus___Team_23.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
