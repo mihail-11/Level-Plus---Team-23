4e4175d2fe3803960a579b437fb2d511223a31ba
x2Dzi <bojan.dukovski211@gmail.com>
2023-04-14 21:12:05 +0200

    adding init migration and creation of local database

--- Level plus - Team 23/Data/Migrations/20230414190814_init.Designer.cs
+++ Level plus - Team 23/Data/Migrations/20230414190814_init.Designer.cs
@@ -4,14 +4,16 @@ using Level_plus___Team_23.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Level_plus___Team_23.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230414190814_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
